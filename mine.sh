895d61c554f120451a6acbb5ac7a474aa74ac5f2
kuongau <117569320+kuongau@users.noreply.github.com>
2023-11-24 09:46:02 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 name = hostname
 ./xmrig -o zephyr.miningocean.org:5332 -u ZEPHYR3C4Nk2s7k281sBxkcEb2T6EKdoJ8CRCwAqopUrV6XidfTXb2mXrR8r4sRRdY1FX3y5Q4FymCnFTCeJHoDe7ZPF5Vor3PK1K -p $name -a rx/0 -k -t 2
